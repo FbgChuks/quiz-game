04a402b7925652f7fc03ecb15896624d97df5fff
Chukwuemeka ogbonna Fbg <120140727+FbgChuks@users.noreply.github.com>
2025-05-19 04:50:01 -0700

    Add files via upload

---
+++ quiz_brain.py
@@ -0,0 +1,33 @@
+class QuizBrain:
+
+
+    def __init__(self, question_list):
+        self.question_number = 0
+        self.score = 0
+        self.question_list = question_list
+
+    def still_has_questions(self):
+        if self.question_number < len(self.question_list):
+            return True
+        else:
+
+            print("\nThanks for playing, you have completed the Game")
+            print(f"Your final score is {self.score}/{len(self.question_list)}")
+            return False
+
+    def next_question(self):
+
+        current_question = self.question_list[self.question_number]
+        self.question_number += 1
+        user_answer = input(f"Q.{self.question_number}. {current_question.text} (True/False?)")
+        self.check_answer(user_answer, current_question.answer)
+
+    def check_answer(self,user_answer, correct_answer):
+           if user_answer.lower()  == correct_answer.lower() :
+               self.score += 1
+               print( "You're correct!")
+           else:
+               print( "Better luck next time!")
+           print(f"The correct answer is : {correct_answer}")
+           print(f"Your current score is :{self.score}/{self.question_number}")
+
